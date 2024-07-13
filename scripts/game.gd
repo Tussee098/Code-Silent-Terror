1b6dbdb333e6261c17708076f32a990509e068ff
Tussee098 <Tusse098@hotmail.com>
2024-07-13 14:12:54 +0200

    Hides UI

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,8 +3,9 @@ extends Node3D
 var peer = ENetMultiplayerPeer.new()
 @export var player_scene : PackedScene
 
-
 func _on_host_button_pressed():
+	_toggle_UI(false)
+	peer.close()
 	peer.create_server(135)
 	multiplayer.multiplayer_peer = peer
 	multiplayer.peer_connected.connect(add_player)
@@ -20,9 +21,18 @@ func add_player(id):
 # MÅste testas
 func _on_join_button_pressed():
 	var ip_adress = $IpAdress.text
+	peer.close()
 	peer.create_client(ip_adress, 135)
 	multiplayer.multiplayer_peer = peer
 	
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
+		peer.close()
 		get_tree().quit()
+		
+
+func _toggle_UI(value : bool):
+	$HostButton.set_visible(value)
+	$JoinButton.set_visible(value)
+	$IpAdress.set_visible(value)
+
