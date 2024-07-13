3f613111290891c143737162940d86a61421983c
PontusEkstrom02 <113009598+PontusEkstrom02@users.noreply.github.com>
2024-07-13 15:10:53 +0200

    addded comments

--- scripts/game.gd
+++ scripts/game.gd
@@ -6,7 +6,7 @@ var players : Array
 
 func _host_button_pressed():
 	_toggle_UI(false)
-	peer.close()
+	peer.close() # idk
 	peer.create_server(135)
 	multiplayer.multiplayer_peer = peer
 	multiplayer.peer_connected.connect(add_player)
@@ -16,7 +16,7 @@ func _host_button_pressed():
 # MÅste testas
 func _join_button_pressed(ip_adress):
 	_toggle_UI(false)
-	peer.close()
+	peer.close() # idk
 	peer.create_client(ip_adress, 135)
 	multiplayer.multiplayer_peer = peer
 
@@ -29,7 +29,7 @@ func add_player(id):
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
-		peer.close()
+		peer.close() # idk
 		get_tree().quit()
 		
 
